0209b23d4439d26a2a4a64355f70a3fc3e168c9b
xhu6 <143360206+xhu6@users.noreply.github.com>
2024-12-20 02:19:45 +0000

    Create image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use image::{Rgb, RgbImage};
+
+fn f(x: u32, y: u32) -> Rgb<u8> {
+    Rgb([x as u8, y as u8, 0])
+}
+
 fn main() {
-    println!("Hello, world!");
+    let img = RgbImage::from_fn(256, 256, f);
+    img.save("out.png").unwrap();
 }
