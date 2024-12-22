65799bcff0d044ba12e79a77bc1eb76fd47d3b9b
xhu6 <143360206+xhu6@users.noreply.github.com>
2024-12-22 02:16:56 +0000

    Moved rendering to Camera

--- src/camera.rs
+++ src/camera.rs
@@ -1,12 +1,21 @@
 use glam::{dvec3, DVec3};
+use image::{Rgb, RgbImage};
 
-use crate::ray::Ray;
+use crate::{
+    hittable::{Hittable, HittableList},
+    ray::Ray,
+};
 
 pub struct Camera {
     position: DVec3,
     viewport_u: DVec3,
     viewport_v: DVec3,
     top_left: DVec3,
+}
+
+// NOTE: Shouldn't be here but will keep for now
+fn to_rgb(data: DVec3) -> Rgb<u8> {
+    Rgb(data.to_array().map(|x| (x * 255.999).floor() as u8))
 }
 
 impl Camera {
@@ -22,7 +31,6 @@ impl Camera {
         let viewport_u = dvec3(viewport_width, 0.0, 0.0);
         let viewport_v = dvec3(0.0, -viewport_height, 0.0);
         let forward = dvec3(0.0, 0.0, -1.0);
-
         let top_left = position + forward * focal_length - viewport_u / 2.0 - viewport_v / 2.0;
 
         Camera {
@@ -34,9 +42,29 @@ impl Camera {
     }
 
     // x and y are in [0, 1]
-    pub fn sample(&self, u: f64, v: f64) -> Ray {
+    pub fn get_ray(&self, u: f64, v: f64) -> Ray {
         let end = self.top_left + self.viewport_u * u + self.viewport_v * v;
         Ray::new(self.position, end - self.position)
+    }
+
+    pub fn get_pixel(&self, world: &HittableList, ray: &Ray) -> Rgb<u8> {
+        if let Some(hit) = world.hit(&ray, 0.0, f64::MAX) {
+            return to_rgb(hit.normal.map(|x| 0.5 * (x + 1.0)));
+        }
+
+        let a = 0.5 * (ray.direction.y + 1.0);
+        let result = (1.0 - a) * DVec3::ONE + a * dvec3(0.5, 0.7, 1.0);
+        to_rgb(result)
+    }
+
+    pub fn render(&self, world: &HittableList, width: u32, height: u32) -> RgbImage {
+        RgbImage::from_fn(width, height, |x, y| {
+            let ray = self.get_ray(
+                (x as f64 + 0.5) / width as f64,
+                (y as f64 + 0.5) / height as f64,
+            );
+            self.get_pixel(world, &ray)
+        })
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
 use core::f64;
 use glam::{dvec3, DVec3};
-use image::{Rgb, RgbImage};
 
-use crate::{
-    camera::Camera,
-    hittable::{Hittable, HittableList},
-    ray::Ray,
-    sphere::Sphere,
-};
+use crate::{camera::Camera, hittable::HittableList, sphere::Sphere};
 
 mod camera;
 mod hittable;
@@ -20,28 +14,6 @@ const HEIGHT: u32 = 1024;
 const VIEWPORT_WIDTH: f64 = 4.0;
 const VIEWPORT_HEIGHT: f64 = VIEWPORT_WIDTH * HEIGHT as f64 / WIDTH as f64;
 
-fn to_rgb(data: DVec3) -> Rgb<u8> {
-    Rgb(data.to_array().map(|x| (x * 255.999).floor() as u8))
-}
-
-fn get_colour(world: &HittableList, ray: Ray) -> Rgb<u8> {
-    if let Some(hit) = world.hit(&ray, 0.0, f64::MAX) {
-        return to_rgb(hit.normal.map(|x| 0.5 * (x + 1.0)));
-    }
-
-    let a = 0.5 * (ray.direction.y + 1.0);
-    let result = (1.0 - a) * DVec3::ONE + a * dvec3(0.5, 0.7, 1.0);
-    to_rgb(result)
-}
-
-fn f(world: &HittableList, cam: &Camera, x: u32, y: u32) -> Rgb<u8> {
-    let ray = cam.sample(
-        (x as f64 + 0.5) / WIDTH as f64,
-        (y as f64 + 0.5) / HEIGHT as f64,
-    );
-    get_colour(world, ray)
-}
-
 fn main() {
     let mut world = HittableList::new();
 
@@ -49,6 +21,6 @@ fn main() {
     world.add(Sphere::new(dvec3(0.0, -100.5, -1.0), 100.0));
 
     let cam = Camera::new(1.0, VIEWPORT_WIDTH, VIEWPORT_HEIGHT, DVec3::ZERO);
-    let img = RgbImage::from_fn(WIDTH, HEIGHT, |x, y| f(&world, &cam, x, y));
+    let img = cam.render(&world, WIDTH, HEIGHT);
     img.save("out.png").unwrap();
 }
