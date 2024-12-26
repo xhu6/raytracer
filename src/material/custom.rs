8c4c741ece531dc56be12f656f75bb53eaee0f9c
xhu6 <143360206+xhu6@users.noreply.github.com>
2024-12-26 02:20:55 +0000

    Use num-complex instead

--- src/material/custom.rs
+++ src/material/custom.rs
@@ -2,7 +2,8 @@ use super::traits::Material;
 
 use crate::{hittable::Hit, ray::Ray};
 
-use glam::{dvec2, dvec3, DVec2, DVec3, Vec3Swizzles};
+use glam::{dvec3, DVec3};
+use num_complex::Complex64;
 
 pub struct Custom {}
 
@@ -12,30 +13,15 @@ impl Custom {
     }
 }
 
-fn cmul(a: DVec2, b: DVec2) -> DVec2 {
-    dvec2(a.x * b.x - a.y * b.y, a.x * b.y + a.y * b.x)
-}
-
-fn conjugate(a: DVec2) -> DVec2 {
-    dvec2(a.x, -a.y)
-}
-
-fn cinv(a: DVec2) -> DVec2 {
-    conjugate(a) / a.length_squared()
-}
-
-fn cdiv(a: DVec2, b: DVec2) -> DVec2 {
-    cmul(a, cinv(b))
-}
-
 fn fractal(point: DVec3) -> DVec3 {
-    let mut z = point.xz() / 16.0;
+    let mut z = Complex64::new(point.x, point.z);
+    let a = Complex64::new(1.0, 1.0);
 
     for _ in 0..32 {
-        z -= cmul(cdiv(cmul(z, z) - dvec2(1.0, 0.0), 2.0 * z), DVec2::ONE);
+        z -= (z * z - 1.0) / (2.0 * z) * a;
     }
 
-    dvec3(z.x.abs(), z.y.abs(), z.y)
+    dvec3(z.re.abs(), z.im.abs(), z.im)
 }
 
 impl Material for Custom {
